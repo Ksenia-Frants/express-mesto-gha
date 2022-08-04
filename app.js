1aaad4596cca868fd8418677973bafaf4f7cb614
Ksenia Frants <frants.ksenka@gmail.com>
2022-08-04 16:25:54 +0700

    Подключиться к Mongo

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+app.listen(3000);
