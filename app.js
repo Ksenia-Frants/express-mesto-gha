6a504b77952143968e6fac971163041f9d2d60a0
Ksenia Frants <frants.ksenka@gmail.com>
2022-09-06 20:05:12 +0700

    Создать роуты для логина и регистрации, сделать мидлвэр для авторизации

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const helmet = require('helmet');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const router = require('./routes');
+const helmet = require("helmet");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("./routes");
+
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 });
 
@@ -16,7 +19,7 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '62eff4b2a051020c723aaf72',
+    _id: "62eff4b2a051020c723aaf72",
   };
 
   next();
@@ -24,6 +27,12 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/signin", login);
+app.post("./signup", createUser);
+
+app.use(auth);
+
 app.use(router);
 
 app.listen(PORT);
